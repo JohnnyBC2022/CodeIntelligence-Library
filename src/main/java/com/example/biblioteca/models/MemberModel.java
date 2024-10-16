8c17971dea484ec4535c96208d9c4a7d4cbb5f09
Johnny <jonathanbc2012@gmail.com>
2024-10-16 19:53:21 +0200

    feat: MemberModel created

---
+++ src/main/java/com/example/biblioteca/models/MemberModel.java
@@ -0,0 +1,60 @@
+package com.example.biblioteca.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "MEMBER")
+public class MemberModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idMember;
+
+    private String dni;
+
+    private String address;
+
+    private String phone;
+
+    public Integer getIdMember() {
+        return idMember;
+    }
+
+    public void setIdMember(Integer idMember) {
+        this.idMember = idMember;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public MemberModel() {
+    }
+
+    public MemberModel(Integer idMember, String dni, String address, String phone) {
+        this.idMember = idMember;
+        this.dni = dni;
+        this.address = address;
+        this.phone = phone;
+    }
+}
