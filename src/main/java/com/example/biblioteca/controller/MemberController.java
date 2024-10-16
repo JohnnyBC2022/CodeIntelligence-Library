4410f114545a51daddfe511784534c8bd0274a89
Johnny <jonathanbc2012@gmail.com>
2024-10-16 20:27:44 +0200

    feat: MemberController created. Testing in Postman OK

---
+++ src/main/java/com/example/biblioteca/controller/MemberController.java
@@ -0,0 +1,45 @@
+package com.example.biblioteca.controller;
+
+import com.example.biblioteca.models.MemberModel;
+import com.example.biblioteca.services.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/members")
+public class MemberController {
+    @Autowired
+    MemberService memberService;
+
+    @PostMapping(value = "/save")
+    public MemberModel saveMember(@RequestBody MemberModel member){
+        MemberModel result = new MemberModel();
+
+        result = memberService.saveMember(member);
+
+        return result;
+    }
+
+    @GetMapping(value = "/get/{id}")
+    public MemberModel getMemberById(@PathVariable(value = "id") Integer id){
+        MemberModel result = new MemberModel();
+
+        result = memberService.getMemberById(id);
+
+        return result;
+    }
+
+    @GetMapping
+    public List<MemberModel> getAllMembers(){
+        return memberService.getAllMembers();
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<Void> deleteMemberById(@PathVariable(value = "id") Integer id){
+        memberService.deleteMemberById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
