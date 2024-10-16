080901e4be441dfcd6b6c3cfbaee4ce67ce04675
Johnny <jonathanbc2012@gmail.com>
2024-10-16 20:01:54 +0200

    feat: MemberService created

---
+++ src/main/java/com/example/biblioteca/services/MemberService.java
@@ -0,0 +1,15 @@
+package com.example.biblioteca.services;
+
+import com.example.biblioteca.models.MemberModel;
+
+import java.util.List;
+
+public interface MemberService {
+    public MemberModel saveMember(MemberModel member);
+
+    public MemberModel getMemberById(Integer id);
+
+    public List<MemberModel> getAllMembers();
+
+    public void deleteMemberById(Integer id);
+}
